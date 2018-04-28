60a05a1571aefa3c18099d1a08bcdf0675054dd1
jesuiscamille <13921610+jesuiscamille@users.noreply.github.com>
2018-04-28 18:00:24 +0200

    Added an install script and renamed main.py to autosqli.py

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# From AutoSQLi
+
+# This script install the requirements of DorkNet and WhatWaf
+pip install -r DorkNet/requirements.txt
+pip install -r WhatWaf/requirements.txt
+
+echo ""
+echo "----------------------------"
+echo "| dependencies installed ! |"
+echo "----------------------------"
+echo ""
