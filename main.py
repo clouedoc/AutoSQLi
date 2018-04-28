0665baf2f64db58532e7f7104656deb030b449ed
jesuiscamille <13921610+jesuiscamille@users.noreply.github.com>
2018-04-28 17:55:36 +0200

    Added a colorized log method

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from src import log
+
+def main():
+    log.debug("test")
+
+if __name__ == "__main__":
+    main()
---
+++ src/log.py
@@ -0,0 +1,24 @@
+# From AutoSQLi
+
+from . import colors
+
+DEBUG_PREFIX = colors.OKBLUE + "[DEBUG] "
+ERROR_PREFIX = colors.FAIL + "[ERROR] "
+INFO_PREFIX = colors.OKBLUE + colors.BOLD + "[INFO] "
+CRITICAL_PREFIX = colors.FAIL + colors.UNDERLINE + colors.BOLD + "[CRITICAL] "
+WARNING_PREFIX = colors.FAIL + colors.BOLD + "[WARNING] "
+
+def debug(msg):
+    print(DEBUG_PREFIX + msg + colors.ENDC)
+
+def info(msg):
+    print(INFO_PREFIX + msg + colors.ENDC)
+
+def error(msg):
+    print(ERROR_PREFIX + msg + colors.ENDC)
+
+def critical(msg):
+    print(CRITICAL_PREFIX + msg + colors.ENDC)
+
+def warning(msg):
+    print(WARNING_PREFIX + msg + colors.ENDC)
