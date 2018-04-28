bc049eeebb3a6347eb10feababa8210501882749
jesuiscamille <13921610+jesuiscamille@users.noreply.github.com>
2018-04-28 18:13:37 +0200

    Removed DorkNet. I'll find an alternative. Problem: it gets only the first ten results, and AutoSQLi will need a lot to work with

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # From AutoSQLi
 
 # This script install the requirements of DorkNet and WhatWaf
+# And also chmod things up
 pip install -r DorkNet/requirements.txt
 pip install -r WhatWaf/requirements.txt
 
@@ -10,3 +11,20 @@ echo "----------------------------"
 echo "| dependencies installed ! |"
 echo "----------------------------"
 echo ""
+
+#chmoding
+
+echo "running chmod +x on *.py ..."
+chmod +x DorkNet/dorknet.py
+chmod +x WhatWaf/WhatWaf.py
+chmod +x sqlmap/sqlmap.py
+
+# installation of geckodriver
+
+echo "installing geckodriver..."
+# since we are pretty dumb as a shell programmer, just bulkly launch install commands with sudo privileges
+echo "Don't mind if there are errors, that's ok. You should get two of them"
+echo "If you're on macOS make sure that brew is in your PATH"
+sudo pacman -S geckodriver
+sudo apt-get install geckodriver
+brew install geckodriver
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from src import log
+
+def DorkNet(dork):
+    
+
+
+def main():
+    log.debug("Test to get some urls out of DorkNet")
+    log.info(DorkNet("chaton mignon"))
+
+if __name__ == "__main__":
+    main()
