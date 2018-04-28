cbc5abacb137c7c22cc0cd3f16a969a85c8349b7
jesuiscamille <13921610+jesuiscamille@users.noreply.github.com>
2018-04-28 19:20:22 +0200

    neovim created a new main.py ^^'

--- autosqli.py
+++ autosqli.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
+import argparse
+
 from src import log
+from src import findDorks
+
+def search_dork(dork):
+    return findDorks.dorkLines(([dork]))
+
+def define_arguments():
+    parser = argparse.ArgumentParser(
+            usage="python autosqli.py [-d DORK] [-f DORKFILE]"
+            )
+
+    parser.add_argument("-f", "--dork-file", metavar="dorkfile.txt", dest=dorkfile, help="if you already dorked, set a file where your dorks are contained ( one dork per line )")
+    
 
 def main():
-    log.debug("test")
+    log.info("Welcome into AutoSQLi !")
+    log.info("Enter a dork:")
+    dork = input("dork: ")
+    url_to_test = search_dork(dork)
 
 if __name__ == "__main__":
     main()
